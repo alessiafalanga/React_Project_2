420c0a6b5476fad090eec264f2a7a600669169a0
“Alessia <a.falanga@eambientegroup.com>
2023-07-31 18:25:51 +0200

    aggiunge css a ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
-function Expenseltem() {
-    return  
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
     // in un return ci può essere solo una radice div. Se si ha bisogno di più radici div, è necessario inserirle tutte in un unico div.
-    <div>  
+    <div className='expense-item'>  
         <div>Date</div>
-        <div>
+        <div className='expense-item__description'>
             <h2>Title</h2>
-            <div>Amount</div>
+            <div className='expense-item__price'>Amount</div>
         </div>
-    </div>;
+    </div>
+    );
 }
 
-export default Expenseltem;
+export default ExpenseItem;
 //non aggiungo in index.js perchè non voglio che si ripeta.
 //aggiungo il compenente nel codice HTML in APP
 
